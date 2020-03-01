e18be65b0a4f55120a7b14760695d1399bc51ca5
budelewski.damian@gmail.com <budelewski.damian@gmail.com>
2020-03-01 22:10:24 +0000

    Update terraform output variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,12 @@
-data "azurerm_public_ip" "shinyapppublicip" {
-    name		= azurerm_public_ip.shinyapppublicip.name
-    resource_group_name	= azurerm_virtual_machine.shinyappvm.resource_group_name
+data "azurerm_public_ip" "publicip" {
+    name		= azurerm_public_ip.publicip.name
+    resource_group_name	= azurerm_virtual_machine.vm.resource_group_name
 }
 
 output "public_ip_address" {
-    value		= data.azurerm_public_ip.shinyapppublicip.ip_address
+    value		= data.azurerm_public_ip.publicip.ip_address
 }
 
 output "fqdn" {
-    value		= data.azurerm_public_ip.shinyapppublicip.fqdn
+    value		= data.azurerm_public_ip.publicip.fqdn
 }
