27d8f29fd869f6673ae5f9b3816b2dde0f43ee2a
budelewski.damian@gmail.com <budelewski.damian@gmail.com>
2020-03-03 22:24:01 +0000

    More complex HA system

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,24 @@ provider "azurerm" {
   version = "= 1.33.0"
 }
 
+# Locals block for hardcoded names. 
+locals {
+    backend_address_pool_name      = "${azurerm_virtual_network.network.name}-beap"
+    frontend_ip_configuration_name = "${azurerm_virtual_network.network.name}-feip"
+    frontend_port_name             = "${azurerm_virtual_network.network.name}-feport"
+    http_setting_name              = "${azurerm_virtual_network.network.name}-be-htst"
+    listener_name                  = "${azurerm_virtual_network.network.name}-httplstn"
+    request_routing_rule_name      = "${azurerm_virtual_network.network.name}-rqrt"
+}
+
+# Generate random string for jumpbox fqdn
+resource "random_string" "fqdn" {
+    length  = 6
+    special = false
+    upper   = false
+    number  = false
+}
+
 # Create a resource group if it doesnt exist
 resource "azurerm_resource_group" "resourcegroup" {
     name     = var.bitcoingtrends["rg_name"]
@@ -10,35 +28,67 @@ resource "azurerm_resource_group" "resourcegroup" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "network" {
-    name                = "virtual-network"
+    name                = "vnet"
     address_space       = ["10.254.0.0/16"]
     resource_group_name = azurerm_resource_group.resourcegroup.name
     location		= azurerm_resource_group.resourcegroup.location
 }
 
 # Create frontend subnet.
-resource "azurerm_subnet" "subnet_frontend" {
-    name                 = "subnet"
+resource "azurerm_subnet" "frontend" {
+    name                 = "back_subnet"
     resource_group_name  = azurerm_resource_group.resourcegroup.name
     virtual_network_name = azurerm_virtual_network.network.name
     address_prefix       = "10.254.0.0/24"
 }
 
 # Create backend subnet.
-resource "azurerm_subnet" "subnet_backend" {
-    name                 = "subnet"
+resource "azurerm_subnet" "backend" {
+    name                 = "front_subnet"
     resource_group_name  = azurerm_resource_group.resourcegroup.name
     virtual_network_name = azurerm_virtual_network.network.name
     address_prefix       = "10.254.2.0/24"
 }
 
-# Create public IPs
+# Create public IP for application gateway
 resource "azurerm_public_ip" "publicip" {
     name		= "publicip"
-    allocation_method	= "Dynamic"
+    allocation_method	= "Static"
     domain_name_label	= var.bitcoingtrends["fqdn"]
     resource_group_name	= azurerm_resource_group.resourcegroup.name
     location		= azurerm_resource_group.resourcegroup.location
+    sku			= "Standard"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "networksecurity" {
+    name		= "nsg"
+    resource_group_name	= azurerm_resource_group.resourcegroup.name
+    location		= azurerm_resource_group.resourcegroup.location
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 999
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 # Create network interfaces
@@ -50,9 +100,9 @@ resource "azurerm_network_interface" "nic" {
     network_security_group_id	= azurerm_network_security_group.networksecurity.id
 
     ip_configuration {
-        name				= "nic-2-config"
-        subnet_id			= azurerm_subnet.subnet_backend.id
-	private_ip_address		= "10.254.2.${count.index}"
+        name				= "nic-config"
+        subnet_id			= azurerm_subnet.backend.id
+	private_ip_address		= "10.254.2.${count.index + 6}"
         private_ip_address_allocation	= "Static"
     }
 }
@@ -101,7 +151,7 @@ resource "azurerm_virtual_machine" "test" {
     }
 
     os_profile {
-        computer_name  = "btcgtrends"
+        computer_name  = "btcgtrends${count.index}"
     	admin_username = "dbudelewski"
     }
 
@@ -109,12 +159,184 @@ resource "azurerm_virtual_machine" "test" {
         disable_password_authentication = true
         ssh_keys {
             key_data	= file("~/.ssh/id_rsa.pub")
-            path	= "~/.ssh/authorized_keys"
+            path	= "/home/dbudelewski/.ssh/authorized_keys"
         }
     }
 
     boot_diagnostics {
         enabled		= "true"
         storage_uri	= azurerm_storage_account.storageaccount.primary_blob_endpoint
+    }
+}
+
+
+resource "azurerm_application_gateway" "gateway" {
+    name                = "app-gw"
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+    location		= azurerm_resource_group.resourcegroup.location
+
+    sku {
+        name     = "Standard_v2"
+        tier     = "Standard_v2"
+        capacity = 2
+    }
+
+    gateway_ip_configuration {
+        name      = "app-gw-cfg"
+        subnet_id = azurerm_subnet.frontend.id
+    }
+
+    frontend_port {
+        name = local.frontend_port_name
+        port = 80
+    }
+    
+    frontend_port {
+        name = "httpsPort"
+        port = 443
+    }
+    
+    frontend_ip_configuration {
+        name                 = local.frontend_ip_configuration_name
+        public_ip_address_id = azurerm_public_ip.publicip.id
+    }
+    
+    backend_address_pool {
+        name = local.backend_address_pool_name
+    }
+    
+    backend_http_settings {
+        name                  = local.http_setting_name
+        cookie_based_affinity = "Disabled"
+        port                  = 80
+        protocol              = "Http"
+        request_timeout       = 1
+    }
+    
+    http_listener {
+        name                           = local.listener_name
+        frontend_ip_configuration_name = local.frontend_ip_configuration_name
+        frontend_port_name             = local.frontend_port_name
+        protocol                       = "Http"
+    }
+    
+    request_routing_rule {
+        name                       = local.request_routing_rule_name
+        rule_type                  = "Basic"
+        http_listener_name         = local.listener_name
+        backend_address_pool_name  = local.backend_address_pool_name
+        backend_http_settings_name = local.http_setting_name
+    }
+    
+    depends_on = ["azurerm_virtual_network.network", "azurerm_public_ip.publicip"]
+}
+
+# Storage share for grafana instance volume
+resource "azurerm_storage_share" "grafana" {
+    name                 = "grafana"
+    storage_account_name = azurerm_storage_account.storageaccount.name
+    quota                = 50
+}
+
+# Create network profile to attach grafana instance into vnet
+resource "azurerm_network_profile" "grafana" {
+    name                = "grafana-net-profile"
+    location            = azurerm_resource_group.resourcegroup.location
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+    
+    container_network_interface {
+        name = "grafana-nic"
+        
+        ip_configuration {
+            name      = "grafana-nic-cfg"
+            subnet_id = azurerm_subnet.backend.id
+        }
+    }
+}
+
+# Create grafana container instance
+resource "azurerm_container_group" "grafana" {
+    name                = "grafana-aci"
+    location            = azurerm_resource_group.resourcegroup.location
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+    ip_address_type     = "private"
+    network_profile_id  = azurerm_network_profile.grafana.id
+    os_type             = "Linux"
+    
+    container {
+        name   = "grafana"
+        image  = "microsoft/aci-helloworld:latest"
+        cpu    = "0.5"
+        memory = "1.5"
+        
+        volume {
+            name = "grafana-volume"
+            storage_account_name = azurerm_storage_account.storageaccount.name
+            storage_account_key = azurerm_storage_account.storageaccount.primary_access_key
+            share_name = azurerm_storage_share.grafana.name
+            mount_path = "/var/lib/grafana/"
+        }
+        
+        ports {
+            port     = 80
+            protocol = "TCP"
+        }
+    }
+}
+
+# Jumpbox configuration
+resource "azurerm_public_ip" "jumpbox" {
+    name				= "jumpbox-public-ip"
+    location				= azurerm_resource_group.resourcegroup.location
+    resource_group_name			= azurerm_resource_group.resourcegroup.name
+    allocation_method			= "Static"
+    domain_name_label			= "${random_string.fqdn.result}-ssh"
+}
+
+resource "azurerm_network_interface" "jumpbox" {
+    name				= "jumpbox-nic"
+    location				= azurerm_resource_group.resourcegroup.location
+    resource_group_name			= azurerm_resource_group.resourcegroup.name
+    
+    ip_configuration {
+    	name				= "IPConfiguration"
+    	subnet_id			= azurerm_subnet.backend.id
+    	private_ip_address_allocation	= "dynamic"
+    	public_ip_address_id		= azurerm_public_ip.jumpbox.id
+    }
+}
+
+resource "azurerm_virtual_machine" "jumpbox" {
+    name				= "jumpbox"
+    location				= azurerm_resource_group.resourcegroup.location
+    resource_group_name			= azurerm_resource_group.resourcegroup.name
+    network_interface_ids		= [azurerm_network_interface.jumpbox.id]
+    vm_size 				= "Standard_B1ms"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04-LTS"
+        version   = "latest"
+    }
+    
+    storage_os_disk {
+        name              = "jumpbox-osdisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    os_profile {
+        computer_name  = "jumpbox"
+    	admin_username = "dbudelewski"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            key_data	= file("~/.ssh/id_rsa.pub")
+            path	= "/home/dbudelewski/.ssh/authorized_keys"
+        }
     }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,11 @@
 data "azurerm_public_ip" "publicip" {
     name		= azurerm_public_ip.publicip.name
-    resource_group_name	= azurerm_virtual_machine.vm.resource_group_name
+    resource_group_name	= azurerm_resource_group.resourcegroup.name
+}
+
+data "azurerm_public_ip" "jumpbox" {
+    name		= azurerm_public_ip.jumpbox.name
+    resource_group_name	= azurerm_resource_group.resourcegroup.name
 }
 
 output "public_ip_address" {
@@ -10,3 +15,12 @@ output "public_ip_address" {
 output "fqdn" {
     value		= data.azurerm_public_ip.publicip.fqdn
 }
+
+output "public_ip_address-jumpbox" {
+    value		= data.azurerm_public_ip.jumpbox.ip_address
+}
+
+output "fqdn-jumpbox" {
+    value		= data.azurerm_public_ip.jumpbox.fqdn
+}
+
