0047cde42a015e4bbd1d9ea17b4cfbdbc9fd75d9
budelewski.damian@gmail.com <budelewski.damian@gmail.com>
2020-03-03 17:57:49 +0000

    Multiply instances of virtual machines

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,25 @@ resource "azurerm_resource_group" "resourcegroup" {
 # Create virtual network
 resource "azurerm_virtual_network" "network" {
     name                = "virtual-network"
-    address_space       = ["10.0.0.0/16"]
+    address_space       = ["10.254.0.0/16"]
     resource_group_name = azurerm_resource_group.resourcegroup.name
     location		= azurerm_resource_group.resourcegroup.location
 }
 
-# Create subnet. Define creating vnet before subnet by refering network name in subnet definition
-resource "azurerm_subnet" "subnet" {
+# Create frontend subnet.
+resource "azurerm_subnet" "subnet_frontend" {
     name                 = "subnet"
     resource_group_name  = azurerm_resource_group.resourcegroup.name
     virtual_network_name = azurerm_virtual_network.network.name
-    address_prefix       = "10.0.1.0/24"
+    address_prefix       = "10.254.0.0/24"
+}
+
+# Create backend subnet.
+resource "azurerm_subnet" "subnet_backend" {
+    name                 = "subnet"
+    resource_group_name  = azurerm_resource_group.resourcegroup.name
+    virtual_network_name = azurerm_virtual_network.network.name
+    address_prefix       = "10.254.2.0/24"
 }
 
 # Create public IPs
@@ -33,61 +41,19 @@ resource "azurerm_public_ip" "publicip" {
     location		= azurerm_resource_group.resourcegroup.location
 }
 
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "networksecurity" {
-    name		= "nsg"
-    resource_group_name	= azurerm_resource_group.resourcegroup.name
-    location		= azurerm_resource_group.resourcegroup.location
-    
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    security_rule {
-        name                       = "HTTP"
-        priority                   = 999
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    security_rule {
-        name                       = "HTTPS"
-        priority                   = 1000
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "443"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-}
-
-# Create network interface
+# Create network interfaces
 resource "azurerm_network_interface" "nic" {
-    name			= "nic"
+    count			= 2
+    name			= "nic${count.index}"
     resource_group_name		= azurerm_resource_group.resourcegroup.name
     location			= azurerm_resource_group.resourcegroup.location
     network_security_group_id	= azurerm_network_security_group.networksecurity.id
 
     ip_configuration {
-        name				= "nic-config"
-        subnet_id			= azurerm_subnet.subnet.id
-        private_ip_address_allocation	= "Dynamic"
-        public_ip_address_id		= azurerm_public_ip.publicip.id
+        name				= "nic-2-config"
+        subnet_id			= azurerm_subnet.subnet_backend.id
+	private_ip_address		= "10.254.2.${count.index}"
+        private_ip_address_allocation	= "Static"
     }
 }
 
@@ -101,7 +67,6 @@ resource "random_id" "randomId" {
     byte_length = 8
 }
 
-# Create storage account for boot diagnostics
 resource "azurerm_storage_account" "storageaccount" {
     name                        = "diag${random_id.randomId.hex}"
     resource_group_name		= azurerm_resource_group.resourcegroup.name
@@ -110,28 +75,29 @@ resource "azurerm_storage_account" "storageaccount" {
     account_replication_type    = "LRS"
 }
 
-# Create virtual machine
-resource "azurerm_virtual_machine" "vm" {
-    name 				= "vm"
+# Create virtual machines
+resource "azurerm_virtual_machine" "test" {
+    count				= 2
+    name 				= "vm${count.index}"
     resource_group_name			= azurerm_resource_group.resourcegroup.name
     location 				= azurerm_resource_group.resourcegroup.location
-    network_interface_ids		= [azurerm_network_interface.nic.id]
+    network_interface_ids		= [element(azurerm_network_interface.nic.*.id, count.index)]
     delete_os_disk_on_termination 	= true
     delete_data_disks_on_termination 	= true
     vm_size 				= "Standard_B1ms"
-
-    storage_os_disk {
-        name              = "osdisk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Premium_LRS"
-    }
 
     storage_image_reference {
         publisher = "OpenLogic"
         offer     = "CentOS"
         sku       = "7.7"
         version   = "latest"
+    }
+
+    storage_os_disk {
+        name              = "osdisk${count.index}"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
     }
 
     os_profile {
@@ -143,7 +109,7 @@ resource "azurerm_virtual_machine" "vm" {
         disable_password_authentication = true
         ssh_keys {
             key_data	= file("~/.ssh/id_rsa.pub")
-            path	= "/home/dbudelewski/.ssh/authorized_keys"
+            path	= "~/.ssh/authorized_keys"
         }
     }
 
